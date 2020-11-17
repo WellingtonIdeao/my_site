035ac67f2b16eb517487864b117d7e9c8dbb5c5f
WellingtonIdeao <wellington.ideao@gmail.com>
2020-11-17 15:40:29 -0300

    refatoring of the test.py and ListIndexView passed the tests (tested)

--- blog/tests.py
+++ blog/tests.py
@@ -2,11 +2,17 @@ from django.test import TestCase
 from django.urls import reverse
 from .models import *
 from django.contrib.auth.models import User
+from django.utils.text import slugify
 # Create your tests here.
 
 
-def create_post(author, title, text, category, tag):
-    post = Post.objects.create(author=author, title=title, text=text)
+def create_post(author, title, slug, category, tag):
+    description = 'descryption'
+    body = 'body'
+
+    post = Post.objects.create(
+        author=author, slug=slug, title=title, description=description, body=body,
+    )
     category.post_set.add(post)
     tag.post_set.add(post)
     return post
@@ -16,12 +22,12 @@ def create_user(username, password):
     return User.objects.create(username=username, password=password)
 
 
-def create_category(name):
-    return Category.objects.create(name=name)
+def create_category(name, slug):
+    return Category.objects.create(name=name, slug=slug)
 
 
-def create_tag(name):
-    return Tag.objects.create(name=name)
+def create_tag(name, slug):
+    return Tag.objects.create(name=name, slug=slug)
 
 
 class PostListIndexTest(TestCase):
@@ -30,6 +36,155 @@ class PostListIndexTest(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertQuerysetEqual(response.context['post_list'], [])
+
+    def test_one_post(self):
+        author = create_user('user', 'senha')
+        title = 'post title'
+        slug = slugify(title)
+
+        category = create_category('category name', slugify('category name'))
+        tag = create_tag('tag name', slugify('tag name'))
+
+        create_post(author, title, slug, category, tag)
+
+        url = reverse('blog:index')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['post_list'], ['<Post: post title>'])
+
+    def test_many_post(self):
+        author = create_user('user', 'senha')
+        title = 'post title'
+        title2 = 'post title2'
+        title3 = 'post title3'
+
+        slug = slugify(title)
+        slug2 = slugify(title2)
+        slug3 = slugify(title3)
+
+        category = create_category('category name', slugify('category name'))
+        tag = create_tag('tag name', slugify('tag name'))
+
+        create_post(author, title, slug, category, tag)
+        create_post(author, title2, slug2, category, tag)
+        create_post(author, title3, slug3, category, tag)
+
+        url = reverse('blog:index')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(
+            response.context['post_list'],
+            [
+                '<Post: post title>',
+                '<Post: post title2>',
+                '<Post: post title3>',
+            ]
+        )
+
+    def test_no_search_field_and_no_post(self):
+        url = reverse('blog:index')
+        response = self.client.get(url, {'search': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.get('search'), None)
+
+    def test_no_search_field_and_one_post(self):
+        author = create_user('user', 'senha')
+        title = 'post title'
+        slug = slugify(title)
+
+        category = create_category('category name', slugify('category name'))
+        tag = create_tag('tag name', slugify('tag name'))
+
+        create_post(author, title, slug, category, tag)
+
+        url = reverse('blog:index')
+        response = self.client.get(url, {'search': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.get('search'), None)
+        self.assertQuerysetEqual(response.context['post_list'], ['<Post: post title>'])
+
+    def test_no_search_field_and_many_post(self):
+        author = create_user('user', 'senha')
+        title = 'post title'
+        title2 = 'post title2'
+        title3 = 'post title3'
+
+        slug = slugify(title)
+        slug2 = slugify(title2)
+        slug3 = slugify(title3)
+
+        category = create_category('category name', slugify('category name'))
+        tag = create_tag('tag name', slugify('tag name'))
+
+        create_post(author, title, slug, category, tag)
+        create_post(author, title2, slug2, category, tag)
+        create_post(author, title3, slug3, category, tag)
+
+        url = reverse('blog:index')
+        response = self.client.get(url, {'search': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.get('search'), None)
+        self.assertQuerysetEqual(
+            response.context['post_list'],
+            [
+                '<Post: post title>',
+                '<Post: post title2>',
+                '<Post: post title3>',
+            ]
+        )
+
+    def test_search_not_found(self):
+        url = reverse('blog:index')
+        response = self.client.get(url, {'search': 'test'})
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['post_list'], [])
+
+    def test_search_one_found(self):
+        author = create_user('user', 'senha')
+        title = 'post title'
+        slug = slugify(title)
+
+        category = create_category('category name', slugify('category name'))
+        tag = create_tag('tag name', slugify('tag name'))
+
+        create_post(author, title, slug, category, tag)
+
+        url = reverse('blog:index')
+        response = self.client.get(url, {'search': 'post title'})
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['post_list'], ['<Post: post title>'])
+
+    def test_search_many_found(self):
+        author = create_user('user', 'senha')
+        title = 'post title'
+        title2 = 'post title2'
+        title3 = 'post title3'
+
+        slug = slugify(title)
+        slug2 = slugify(title2)
+        slug3 = slugify(title3)
+
+        category = create_category('category name', slugify('category name'))
+        tag = create_tag('tag name', slugify('tag name'))
+
+        create_post(author, title, slug, category, tag)
+        create_post(author, title2, slug2, category, tag)
+        create_post(author, title3, slug3, category, tag)
+
+        url = reverse('blog:index')
+        response = self.client.get(url, {'search': 'post title'})
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(
+            response.context['post_list'],
+            [
+                '<Post: post title>',
+                '<Post: post title2>',
+                '<Post: post title3>',
+            ]
+        )
+
+
+'''     self.assertQuerysetEqual(response.context['post_list'], [])
 
     def test_one_post(self):
         create_post(
@@ -178,7 +333,7 @@ class PostByCategoryTest(TestCase):
                 '<Post: title>',
             ]
         )
-
+'''
 
 
 

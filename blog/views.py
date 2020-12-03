3d0a546a1e24edc7a253a4c16de4d59264b17756
WellingtonIdeao <wellington.ideao@gmail.com>
2020-12-02 21:47:35 -0300

    refactored base, index and post_list template

--- blog/views.py
+++ blog/views.py
@@ -35,12 +35,17 @@ class PostByTag(ListView):
 
 class PostByCategory(ListView):
     model = Post
-    template_name = 'blog/index.html'
+    template_name = 'blog/post_list.html'
     context_object_name = 'post_list'
-    allow_empty = False
+    # allow_empty = False
 
     def get_queryset(self):
         return Post.objects.filter(categories__slug=self.kwargs['slug'])
+
+    def get_context_data(self, object_list=None, **kwargs):
+            context = super().get_context_data(**kwargs)
+            context['slug'] = self.kwargs['slug']
+            return context
 
 
 '''class PageSelectView(View):
